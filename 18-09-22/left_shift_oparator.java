8b0afd77a3e276eaba2de32fb47e2813b0464771
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-09-18 01:31:51 +0600

    Add files via upload

---
+++ 18-09-22/left_shift_oparator.java
@@ -0,0 +1,10 @@
+public class left_shift_oparator {
+  public static void main (String[]args) {
+    System.out.println(10<<3);// 10*2^3
+    System.out.println(10<<4);//10*2^4
+    System.out.println(10<<5);
+    System.out.println(10<<2);
+    System.out.println(12<<2);
+    
+  }  
+}
---
+++ 18-09-22/right_shift_oparator.java
@@ -0,0 +1,9 @@
+public class right_shift_oparator {
+
+    public static void main(String[]args){
+        System.out.println(10>>2);
+        System.out.println(10>>3);//10/2^3
+        System.out.println(10>>4);//10/2^4
+        System.out.println(12>>2);
+    }
+}
