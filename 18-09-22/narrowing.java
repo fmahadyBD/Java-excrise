09dc40ed41927f46e08aeecb4e8f517b520b9a48
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-09-18 00:07:37 +0600

    Add files via upload

---
+++ 18-09-22/narrowing.java
@@ -0,0 +1,12 @@
+public class narrowing {
+    public static void main(String[] args) throws Exception {
+    System.out.println("hi");
+    float ba=(float) 10.2;
+    int ca=(int) ba;
+    System.out.println(ba);
+    System.out.println(ca);
+
+        
+    }
+    
+}
---
+++ 18-09-22/widding.java
@@ -0,0 +1,11 @@
+public class widding {
+    public static void main(String[] args) {
+        
+        int a=10;
+        float b=a;
+        
+        System.out.println("the the wedding:" +b);
+        
+    }
+    
+}
