e8c0c7c51059a63ba37126aa9bd4499c9310e9c1
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-09-17 23:55:19 +0600

    Add files via upload

---
+++ 17-09-22/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        int a=10;
+        int b=20; 
+        System.out.println("The sum of two number");
+        System.out.println(a+b);
+        System.out.println("The minus of two number");
+        System.out.println(a-b);
+        System.out.println("The division of two number");
+        System.out.println(a/b);
+        System.out.println("The multiplication of two number");
+        System.out.println(a*b);
+        System.out.println("The mudolodivision of two number");
+        System.out.println(a%b);
+        
+    }
+}
