209e3e7eba39dd701e3ea11a29dbe33290163889
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-09-18 01:12:58 +0600

    Add files via upload

---
+++ 18-09-22/unarrayOparator.java
@@ -0,0 +1,18 @@
+public class unarrayOparator{
+    public static void main(String[]args){
+        int a=10;
+        System.out.println("a=10");
+      
+        System.out.println("a++ is: " + a++);
+        System.out.println("a is:" + a);
+        System.out.println("++a is:" + ++a);
+        System.out.println("a is:" + a);
+
+        System.out.println("a-- is:" + a--);
+        System.out.println("a is:" + a);
+        System.out.println("--a is:" + --a);
+        System.out.println("a is:" + a);
+      
+
+    }
+}
